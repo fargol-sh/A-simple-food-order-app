329c3815393e083a1256e05ca2509b96566ee173
Fargol Shirvanifar <fargolsh79@gmail.com>
2023-04-16 19:02:00 +0430

    Add cart reducer, add refs and forward refs, output cart items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,21 +1,42 @@
 import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
+import { isContentEditable } from '@testing-library/user-event/dist/utils';
 
 const Cart = props => {
-    const cartItems = <ul className={classes['cart-items']}>{
-    [{
-        id: 'c1', 
-        name: 'Sushi', 
-        amount: 2, 
-        price: 12.99
-    }].map(item => <li>{item.name}</li>)}</ul>;
+    const cartCtx = useContext(CartContext);
+
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = cartCtx.items.length > 0;
+
+    const cartItemRemoveHandler = id => {
+
+    };
+
+    const cartItemAddHandler = item => {
+
+    };
+
+    const cartItems = <ul className={classes['cart-items']}>
+    {cartCtx.items.map(item => 
+    <CartItem 
+        key={item.id} 
+        name={item.name}
+        amount={item.amount}
+        price={item.price}
+        onRemove={cartItemRemoveHandler.bind(null, item.id)}
+        onAdd={cartItemAddHandler.bind(null, item)}
+    />
+    )}</ul>;
 
     return (
         <Modal onClose={props.onClose}>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmount}</span>
             </div>
 
             <div className={classes.actions}>
@@ -23,7 +44,7 @@ const Cart = props => {
                     className={classes['button--alt']}
                     onClick={props.onClose}
                 >Close</button>
-                <button className={classes.button}>Order</button>
+                {hasItems && <button className={classes.button}>Order</button>}
             </div>
         </Modal>
     );
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,60 @@
 import CartContext from "./cart-context";
+import { useReducer } from 'react';
+
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+};
+
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD') {
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+        const existingCartItemIndex = state.items.findIndex(
+            item => item.id === action.item.id
+        );
+
+        const existingCartItem = state.items[existingCartItemIndex];
+        let updatedItems;
+
+        if (existingCartItem) {
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            };
+
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+            //concat() does not edit the existing array like push(), it creates a new array.
+            updatedItems = state.items.concat(action.item);
+        }
+        
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+    } else {
+
+    }
+
+    return defaultCartState;
+};
 
 const CartProvider = props => {
-    const addItemToCartHandler = item => {};
+    const [cartState, dispathCartAction] = useReducer(cartReducer, defaultCartState);
 
-    const removeItemFromCartHandler = id => {};
+    const addItemToCartHandler = item => {
+        dispathCartAction({type: 'ADD', item: item});
+    };
+
+    const removeItemFromCartHandler = id => {
+        dispathCartAction({type: 'REMOVE', id: id});
+    };
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler
     };
